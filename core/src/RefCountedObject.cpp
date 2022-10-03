1538abbb49499d53e113a661ad6bbfe9dc947e5d
Maya Posch <maya@nyanko.ws>
2022-10-03 23:18:35 +0200

    Revert breaking change.

--- core/src/RefCountedObject.cpp
+++ core/src/RefCountedObject.cpp
@@ -19,24 +19,4 @@ RefCountedObject::~RefCountedObject()
 }
 
 
-int RefCountedObject::referenceCount() const {
-	return _counter.value();
-}
-
-
-void RefCountedObject::duplicate() const {
-	++_counter;
-}
-
-
-void RefCountedObject::release() const {
-	//try 	{
-		if (--_counter == 0) delete this;
-	/*}
-	catch (...) {
-		poco_unexpected();
-	}*/
-}
-
-
 } // namespace Poco
--- core/src/RefCountedObject.h
+++ core/src/RefCountedObject.h
@@ -50,14 +50,24 @@ private:
 //
 // inlines
 //
-/*inline int RefCountedObject::referenceCount() const {
+inline int RefCountedObject::referenceCount() const {
 	return _counter.value();
-}*/
+}
 
 
-/*inline void RefCountedObject::duplicate() const {
+inline void RefCountedObject::duplicate() const {
 	++_counter;
-}*/
+}
+
+
+inline void RefCountedObject::release() const {
+	//try 	{
+		if (--_counter == 0) delete this;
+	/*}
+	catch (...) {
+		poco_unexpected();
+	}*/
+}
 
 
 } // namespace Poco
