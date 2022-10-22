d13a8f93b7e552f8336fc415598cfa3500588fb2
Maya Posch <maya@nyanko.ws>
2022-10-22 11:27:29 +0200

    Include platform source, not header.

--- core/src/RWLock.cpp
+++ core/src/RWLock.cpp
@@ -6,7 +6,7 @@
 #include "RWLock.h"
 
 #if defined(POCO_OS_FAMILY_FREERTOS)
-#include "platforms/RWLock_FreeRTOS.h"
+#include "platforms/RWLock_FreeRTOS.cpp"
 #elif defined(POCO_OS_FAMILY_WINDOWS)
 #if defined(_WIN32_WCE)
 #include "platforms/RWLock_WINCE.cpp"
